e93beb4c9e378173f85f7cce70a1dee440027401
Konstantin Iartsev <web101@web101.ru>
2024-06-16 06:37:10 +0700

    dist only

--- README.md
+++
@@ -1,4 +0,0 @@
-# Mesto-FF The training project sprint 7
-#webstudio101
-
-
--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-// postcss.config.js KN - 7sp
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
-module.exports = {
-  plugins: [autoprefixer, cssnano({ preset: "default" })],
-};
